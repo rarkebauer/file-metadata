48ba8ed02f022969be1de4a791e80852722cb1fa
rarkebauer <rarkebauer@gmail.com>
2017-06-14 21:35:00 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# file-metadata
-File metadata microservice from FCC backend projects
+This is a project from Free Code Camp's backend certification
+
+The user can submit a FormData object that includes a file upload. When the user submits something, he or she will receive the file size in bytes within the JSON response.
---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' })
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+var url = require('./url.js');
+
+function urlRoute(req, res) {
+	var search = req.params[0];
+	res.json(search);
+}
+
+app.listen(port, function() {
+	console.log('Example app listening on port ' + 3000);
+})
+
+app.post('/api/upload', upload.any('fileToSize'), function(req, res, next){
+  var obj = req.files[0];
+  var size = req.files[0].size
+  console.log(req.files[0]);
+  res.json({'size': size});
+});
+
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html')
+})
+
+module.exports = app;
